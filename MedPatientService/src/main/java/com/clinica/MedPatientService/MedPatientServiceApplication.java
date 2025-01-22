df266a11b2b722a462c1758b23fcad5a4677f78b
Nicolas Morales <nicolasmoralesj03@gmail.com>
2025-01-22 13:48:00 -0300

    se agrega servicio medPatientService

---
+++ MedPatientService/src/main/java/com/clinica/MedPatientService/MedPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.MedPatientService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedPatientServiceApplication.class, args);
+	}
+
+}
---
+++ MedPatientService/src/test/java/com/clinica/MedPatientService/MedPatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.MedPatientService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedPatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
