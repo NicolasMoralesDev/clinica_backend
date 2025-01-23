4db313555afa8556f6ce8b004967c8787e39560c
Nicolas Morales <nicolasmoralesj03@gmail.com>
2025-01-23 11:36:08 -0300

    Se crea eureka server

--- MedPatientService/src/main/java/com/clinica/MedPatientService/MedPatientServiceApplication.java
+++ MedPatientService/src/main/java/com/clinica/MedPatientService/MedPatientServiceApplication.java
@@ -2,6 +2,7 @@ package com.clinica.MedPatientService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class MedPatientServiceApplication {
